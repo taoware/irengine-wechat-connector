895c0ea0f4e11b1b28c317abcf4466c04159b539
Alex Tang <alex.tang@taoware.com>
2015-04-17 00:58:05 +0800

    add dummy view

---
+++ src/main/java/com/irengine/wechat/connector/controller/DummyController.java
@@ -0,0 +1,16 @@
+package com.irengine.wechat.connector.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class DummyController {
+
+    @RequestMapping("/dummy")
+    public String dummy(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "dummy";
+    }
+}
