8e329041ec1453c76b9d065a899656d85f824b5a
Alex Tang <alex.tang@taoware.com>
2015-04-17 00:05:21 +0800

    initial version

---
+++ src/main/java/com/irengine/wechat/connector/IrengineWechatConnectorApplication.java
@@ -0,0 +1,12 @@
+package com.irengine.wechat.connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrengineWechatConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrengineWechatConnectorApplication.class, args);
+    }
+}
