3ded06011997e704a584c7dc969627150e900325
Alex Tang <alex.tang@taoware.com>
2015-04-17 00:17:15 +0800

    add application bootstrap class

---
+++ src/main/java/com/irengine/wechat/connector/ApplicationBootstrap.java
@@ -0,0 +1,25 @@
+package com.irengine.wechat.connector;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+	  /*
+	   * This method is called during Spring's startup.
+	   * 
+	   * @param event Event raised when an ApplicationContext gets initialized or
+	   * refreshed.
+	   * 
+	   * http://stackoverflow.com/questions/6684451/executing-a-java-class-at-application-startup-using-spring-mvc
+	   * http://stackoverflow.com/questions/22389996/how-to-configure-spring-boot-servlet-like-in-web-xml
+	   */
+	  @Override
+	  public void onApplicationEvent(final ContextRefreshedEvent event) {
+
+		  System.out.println("===============Bootstrap===============");
+		  return;
+	  }
+}
